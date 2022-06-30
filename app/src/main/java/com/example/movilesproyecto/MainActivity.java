c90c8ff7b76d1144d498d39ffb286263b1ce6ea8
Enma123 <joseemiliogr5@gmail.com>
2022-06-29 18:36:42 -0600

    boton de tomar foto funciona

--- app/src/main/java/com/example/movilesproyecto/MainActivity.java
+++ app/src/main/java/com/example/movilesproyecto/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.movilesproyecto;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
         final EditText edit_fechareporte = findViewById(R.id.edit_fechareporte);
         final EditText edit_severidad = findViewById(R.id.edit_severidad);
         final EditText edit_estado = findViewById(R.id.edit_estado);
+        LinearLayout layoutBtnCamera = (LinearLayout) findViewById(R.id.layoutBtnCamera);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT );
+        //for (int i=0; i<numBotones; i++){
+        img = findViewById(R.id.img);
+        Button button = new Button(this);
+        //Asignamos propiedades de layout al boton
+        button.setLayoutParams(lp);
+        //Asignamos Texto al botón
+        button.setText("Cámara");
+        //Añadimos el botón a la botonera
+        layoutBtnCamera.addView(button);
+        //}
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            }
+        });
+
         Button btn = findViewById(R.id.btn_submit);
         Button btn_open = findViewById(R.id.btn_open);
         btn_open.setOnClickListener(v->
@@ -78,4 +105,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK){
+                Bundle extras =  result.getData().getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                img.setImageBitmap(imgBitmap);
+            }
+        }
+    });
+
 }
